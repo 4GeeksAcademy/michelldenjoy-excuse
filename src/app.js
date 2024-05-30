4d29d50c6353ce33fd128f49f46886c4c40f1349
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-05-30 12:47:58 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My dog", "My Grandma", "My sister", "My mom"];
+  let action = ["broke", "bite", "cut", "hit"];
+  let what = ["my homework", "my phone", "my hair", "the car"];
+  let when = [
+    "while i was sleeping",
+    "while i was playing",
+    "during my lunch",
+    "while i was exercising"
+  ];
+
+  const excuseGenerator = () => {
+    let fraseAleatoria = Math.random();
+    let indexWho = Math.floor(fraseAleatoria * who.length);
+    let indexAction = Math.floor(fraseAleatoria * action.length);
+    let indexWhat = Math.floor(fraseAleatoria * what.length);
+    let indexWhen = Math.floor(fraseAleatoria * when.length);
+
+    let wordWho = who[indexWho];
+    let wordAction = action[indexAction];
+    let wordWhat = what[indexWhat];
+    let wordWhen = when[indexWhen];
+
+    let fraseFinal =
+      wordWho + " " + wordAction + " " + wordWhat + " " + wordWhen;
+    return fraseFinal;
+  };
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
